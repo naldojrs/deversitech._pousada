25df34f50f5ff80b7e84c0c7d60aab63de4539b1
Josinaldo Rodrigues Santos <josinaldo.rodrigues@montcalm.com.br>
2021-10-23 12:54:13 -0300

    Adiconado codigo BD python

---
+++ Codigo Python/banco_dados.py
@@ -0,0 +1,91 @@
+import pymysql.cursors
+import getpass
+
+
+def conecta_bd():
+    print('=' * 31)
+    print('Conectando com o banco de dados')
+    print('=' * 31)
+    print('Por favor, preencha as informações a seguir:')
+
+    servidor = input('Servidor: ')
+    usuario = input('Usuário: ')
+    senha = getpass.getpass('Senha: ')
+    banco = input('Banco: ')
+
+    # Conexão com Banco de Dados
+    conexao = pymysql.connect(
+        host=servidor,  # ip ou o nome da maquina
+        user=usuario,
+        password=senha,
+        database=banco,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    print('Banco de dados conectado com sucesso!')
+    return conexao
+
+
+def cadastra_cliente(cursor, nome, cpf, telefone):
+    comando = f'''
+    INSERT INTO `locadora`.`cliente`(
+        `nome`,
+        `cpf`,
+        `telefone`
+    ) VALUES (
+        "{nome}",
+        "{cpf}",
+        "{telefone}"
+    );
+    '''
+
+    cursor.execute(comando)
+    print('Cadastro executado com sucesso!')
+    print(f'Cliente {nome} cadastrado!')
+    # return None
+
+
+def consulta_cliente(cursor):
+    comando = '''
+    SELECT * FROM cliente
+    '''
+
+    cursor.execute(comando)
+
+    resultado = cursor.fetchall()
+    for dic in resultado:
+        print('-' * 50)
+        print('ID: ', dic['id'])
+        print('Nome: ', dic['nome'])
+        print(f"CPF: {dic['cpf']}")
+        print(f'Telefone: {dic["telefone"]}')
+
+    return None
+
+# cadastro_carro = ''
+# cadastro_aluguel = ''
+
+# cursor.execute(cadastro_cliente)
+# print('Cadastro executado com sucesso!')
+# print(f'Cliente {nome} cadastrado!')
+# conexao.commit()
+
+# Deleção
+# remocao_carro =
+# remocao_cliente =
+
+# Consulta
+# consulta_cliente = f'''
+# SELECT *
+# FROM cliente
+# WHERE nome = "{nome}"
+# ;
+# '''
+# print(consulta_cliente)
+
+# cursor.execute(consulta_cliente)
+# resultado = cursor.fetchall()
+
+# print('Consulta executada com sucesso!')
+# print('Resultado: ')
+# print(resultado)
