536c564d7ca33df30f4440acaeb4c7e2393d1810
Josinaldo Rodrigues Santos <josinaldo.rodrigues@montcalm.com.br>
2021-10-23 12:52:42 -0300

    Incluido mais opções no menu

--- Codigo Python/pousada.py
+++ Codigo Python/pousada.py
@@ -1,48 +1,86 @@
-
-# MENU POUSADA
 import os
-op = ''
-print('='*35)
-print('Bem vindo(a) ao Sistema de Locadora')
-print('='*35)
-print()
-print('Para começar')
-while op != '5': # Sai quando a op é 5
-  print('Escolha uma das opções abaixo:')
-  print('    1 - Cadastro Cliente')
-  print('    2 - Cadastro Quarto')
-  print('    3 - Reserva')
-  print('    4 - Relatório')
-  print('    5 - Sair')
-  op = input('Opção: ')
-  if op == '1':
-    #cadastra_cliente()
-    os.system('clear')
-    print('='*19)
-    print('Cadastro de Cliente')
-    print('='*10)
-    nome = input('Nome: ')
-    cpf = input('CPF: ')
-    telefone = input('Telefone: ')
-    print(f'cadastra_cliente({nome}, {cpf}, {telefone})')
-    os.system('clear')
-    print(f'Usuário {nome} cadastrado com sucesso!')
-  elif op == '2':
-    #cadastra_quarto()
-    print('cadastra_quarto()')
-  elif op == '3':
-    #reserva()
-    print('reserva()')
-  elif op == '4':
-    #relatorio()
-    print('relatorio()')
-  elif op == '5':
-    os.system('clear')
-    print('Obrigado!')
-    print('Volte Sempre!')
-  else:
-    os.system('clear')
-    print('Opção Inválida!')
-    print('Por favor')
 
-print('Fim do Progama!')
+from mysql.connector import cursor
+
+import banco_dados
+
+conexao = banco_dados.conecta_bd()
+cursor = conexao.cursor()
+
+
+def menu(conexao=cursor):
+    os.system('cls')
+    op = ''
+    print('='*35)
+    print('       SISTEMA DE RESERVA')
+    print('='*35)
+    print()
+    while op != '7':  # Sai quando a op é 5
+        print('    ESCOLHA A OPÇÃO DESEJADA:')
+        print('=' * 35)
+        print('    1 - CADASTRO DE CLIENTE')
+        print('    2 - CADASTRO DE QUARTO')
+        print('    3 - EXCLUIR CLIENTE')
+        print('    4 - EXCLUIR QUARTO')
+        print('    5 - RESERVA')
+        print('    6 - Relatório')
+        print('    7 - Sair')
+        op = input('Opção: ')
+        if op == '1':
+            os.system('cls')
+            print('='*19)
+            print('CADASTRA CLIENTE')
+            print('='*10)
+
+            nome = input('Nome: ')
+            cpf = input('CPF: ')
+            telefone = input('Telefone: ')
+
+            banco_dados.cadastra_cliente(cursor, nome, cpf, telefone)
+            conexao.commit()  # se não executar commit as alterações não são salvas
+            os.system('cls')
+            print(f'Usuário {nome} cadastrado com sucesso!')
+        elif op == '2':
+            # CADASTRA QUARTO()
+            print('CADASTRA QUARTO()')
+            os.system('cls')
+            acomodacao = input('Acomodação:')
+            tipo = input('Tipo:')
+            numero = input('Número:')
+            status = input('Status:')
+
+            print(f'Cadastro Quarto({acomodacao}, {tipo}, {numero},{status}')
+            os.system('cls')
+            print(f'Quarto {numero} Cadstrado com Sucesso!')
+        elif op == '3':
+            # EXCLUIR CLIENTE ()
+            print('CLIENTE EXCLUIDO()')
+        elif op == '4':
+            # EXCLUIR QUARTO()
+            print('QUARTO EXCLUIDO()')
+        elif op == '5':
+            # RESERVA()
+            print('RESERVA()')
+        elif op == '4':
+            os.system('cls')
+            banco_dados.consulta_cliente(cursor)
+            print('-'*50)
+            input('Aperte Enter para voltar: ')
+            os.system('cls')
+
+        elif op == '7':
+            os.system('cls')
+            cursor.close()
+            conexao.close()
+            print('Obrigado!')
+            print('Volte Sempre!')
+        else:
+            os.system('cls')
+            print('Opção Inválida!')
+            print('Por favor')
+
+    print('Fim do Progama!')
+
+    return None
+
+# menu()
